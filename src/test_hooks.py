2190e23f4c4cacf1cbd25e7831ae3713d60bfce0
Bangyen Pham <bangyen99@gmail.com>
2025-09-04 13:23:20 -0500

    Remove test files

--- src/test_hooks.py
+++
@@ -1,6 +0,0 @@
-"""Test file to trigger pre-commit hooks."""
-
-
-def test_function_with_types(x: int) -> str:
-    """Test function with type annotations."""
-    return str(x * 2)
--- test_commit.py
+++
@@ -1,3 +0,0 @@
-# Test file to trigger pre-commit hooks
-def test_function():
-    return "Hello, pre-commit!"
